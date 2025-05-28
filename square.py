b8963096c008ae3127b728e10fdf61a917467a89
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-05-28 20:20:09 +0000

    Added versions of square.py to make iterations of my code more clear

--- square.py
+++ square.py
@@ -9,3 +9,15 @@ main()
 
 # Notice we definined main() and square() functions and then called main() at the end of the script.  
 # This is a common pattern in Python scripts to ensure that the code is written in a top-down manner.
+
+def version_1():
+    x = input("Enter a number to square: ")
+    print("x squared is ")
+    print(square(x))
+
+def version_2():
+    # I can make several iterations of my code that improve it and add complexity.
+    pass
+
+def version_3():
+    pass # This is a placeholder for future parts of the program.
