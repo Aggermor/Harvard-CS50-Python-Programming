b7d23547f719ab552e282465dc3495a0aa37582a
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-03 23:03:46 +0000

    cleaned up number.py and added a loop until the user enters a valid number

--- number.py
+++ number.py
@@ -7,17 +7,35 @@
 # How can you know what error might occure in future?
 # --> sometimes documentation of a function might suggest what could go wrong.
 
+def main():
+    version_2()
 
-input = input("Enter an intager for x: ")
+def version_1():
+    usr_input = input("Enter an intager for x: ")   # put outside the try because we don't need to try this part.
 
-try:
-    x = int(input)
-except ValueError:
-    print(f"'{input}' is not an intager!")      # cannot use {x} because it would throw an error before being assigned.
-else:
-    print(f"x is {x}")
+    try:
+        x = int(usr_input) #! this broke when the variable was named `input``, because input is a built-in function. Can I name varibles th same as built-in functions?
+    except ValueError:
+        print(f"'{input}' is not an intager!")      # cannot use {x} because it would throw an error before being assigned.
+    else:
+        print(f"x is {x}")
 
-print(f"I can still use x '{x}'")               # you can still use {x} AFTER an else error handling. NOT BEFORE.
+    print(f"I can still use x '{x}'")               # you can still use {x} AFTER an else error handling. NOT BEFORE.
 
-# https://youtu.be/nLRL_NcnK-4?t=16211
-# around this time?
+
+def version_2():
+    while True:                                     # This will loop until a valid integer is entered.
+        usr_input = input("Enter an intager for n: ")
+        
+        try:
+            n = int(usr_input)
+        except ValueError:
+            print(f"'{usr_input}' is NaN. Please enter a valid integer...")
+            continue                                # continue to the next iteration of the loop
+        else:
+            print(f"{n} is a valid intager.")       # works here
+            break                                   # break out of the loop if no error occurred        
+
+    print(f"{n} is a valid intager.")               # works here too, after the loop has ended.
+
+main()
