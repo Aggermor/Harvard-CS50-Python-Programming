c8281873167045fe98a59ce2c4bfd14ae1094e3d
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-04 16:09:40 +0000

    cleaned up the code and added a cleaner version that is arguably less Pythonic

--- name.py
+++ name.py
@@ -1,3 +1,4 @@
+# To run this script, use the command: `python name.py <your_name>`
 # sys.argv[0] is the script name
 # sys.argv[1] is the first argument passed to the script
 # sys.argv[2] is the second argument passed to the script etc...
@@ -15,8 +16,19 @@ except IndexError:
 
 # We can also handle the case where both first and last names are provided
 try:
-    argv[1], argv[2]
+    argv[1], argv[2] # This will raise an IndexError if there are not enough command line arguments
 except IndexError:
     pass
 else:
     print(f"Hello, my name is {argv[1]} {argv[2]}!")
+
+
+""" # Implementation of cleaner code that handles command line arguments. It's Pythonic to handle errors rather than using if-else statements.
+from sys import argv
+
+if len(argv) == 1: # Check if no command line arguments are provided, `if not argv[1:]` would also work
+    print("Please provide your name as a command line argument.")
+else:
+    full_name = " ".join(argv[1:])
+    print(f"Hello, my name is {full_name}.")
+"""
