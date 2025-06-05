3d9dd682810558e748a0f7f4bbc0256c8656a2ac
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-05 16:07:15 +0000

    Added cowsay last commit, but wrote some notes for it before I program with cowsay.

---
+++ cowsay.py
@@ -0,0 +1,8 @@
+# https://youtu.be/nLRL_NcnK-4?t=20185
+# Packages
+# PyPI (Python Package Index), is a repository of software for the Python programming language.
+# cowsay is a program that generates ASCII art of a cow with a speech bubble containing a message.
+# pip is a program called a package manager for Python that allows you to install and manage Python packages.
+# $ pip install cowsay
+
+import cowsay
