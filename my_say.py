0f0ad5976969a7b14058475a25eed94140ec0293
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-06 01:27:15 +0000

    getting ready to sign off for the day, __name__ is very interesting to me and a more difficult concept to grasp

--- my_say.py
+++ my_say.py
@@ -1,4 +1,9 @@
 import sys
 from sayings import hello
 
-hello("world")
+if len(sys.argv) == 2:
+    name = sys.argv[1]
+    hello(name)
+else:
+    print("Usage: `$ python my_say.py <name>`")
+    sys.exit(1) # exit(1) is used to indicate that the program has terminated with an error.
--- sayings.py
+++ sayings.py
@@ -1,8 +1,18 @@
 # https://youtu.be/nLRL_NcnK-4?t=21757
 # I created my own module named `sayings.py` used in the file `my_say.py` to demonstrate how to import and use functions from a custom module.
+def main():
+    hello("world")
+    goodbye("world")
 
 def hello(name):
     print(f"hello, {name}")
 
 def goodbye(name):
     print(f"goodbye, {name}")
+
+if __name__ == "__main__":
+    main()
+
+# https://youtu.be/nLRL_NcnK-4?t=22013
+# The `if __name__ == "__main__":` block is used to ensure that the `main()` function is only called when the script is run directly, not when it is imported as a module in another script.
+# `__name__` is a special variable in Python that is set to "__main__" when the script is run directly.
