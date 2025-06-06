c08949ce9cff1e2e5863f83f2d6a88ae7e578560
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-06 15:34:30 +0000

    Finished notes for __name__. Cleaned up square.py to get ready for unit testing it using test_square.py

--- sayings.py
+++ sayings.py
@@ -17,3 +17,10 @@ if __name__ == "__main__":
 # https://youtu.be/nLRL_NcnK-4?t=22013
 # The `if __name__ == "__main__":` block is used to ensure that the `main()` function is only called when the script is run directly, not when it is imported as a module in another script.
 # `__name__` is a special variable in Python that is set to "__main__" when the script is run directly.
+
+# When you run this script directly, `__name__` will be "__main__".
+# When you import this script as a module in another script, `__name__` will be the name of the module (in this case, "sayings").
+
+# print(__name__)
+# 'saysings' will print if the module is imported.          --> Demonstrating __name__ will == 'sayings'
+# '__main__' will print if the module is run directly.      --> Demonstrating __name__ will == '__main__'
--- square.py
+++ square.py
@@ -5,19 +5,6 @@ def main():
 def square(n):
     return int(n) ** 2 # `**` Exponentiation operator for powering
 
-main()
-
-# Notice we definined main() and square() functions and then called main() at the end of the script.  
-# This is a common pattern in Python scripts to ensure that the code is written in a top-down manner.
-
-def version_1():
-    x = input("Enter a number to square: ")
-    print("x squared is ")
-    print(square(x))
-
-def version_2():
-    # I can make several iterations of my code that improve it and add complexity.
-    pass
-
-def version_3():
-    pass # This is a placeholder for future parts of the program.
+if __name__ == "__main__":
+    # This block ensures that the main function is called only when the script is run directly.
+    main()
