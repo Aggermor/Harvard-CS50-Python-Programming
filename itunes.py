36f3f07e1486b64b9df0de7828fedf4740597f05
Aggermor <59328078+Aggermor@users.noreply.github.com>
2025-06-06 00:52:50 +0000

    Included an iterator to add a numbered list to the output and found a bug with multi-word artist names.

--- itunes.py
+++ itunes.py
@@ -1,11 +1,14 @@
+# `$ python itunes.py weezer` # This terminal command runs the script with "weezer" as the argument, which will search for songs by the artist Weezer on iTunes.
+# `$ python itunes.py "Avenged Sevenfold"` this did not work because the argv is not properly handled for multi-word artist names?
+# 
 # https://youtu.be/nLRL_NcnK-4?t=20700
 # API (Application Programming Interface) is a set of rules and protocols for building and interacting with software applications. 
 #     --> It allows different software components to communicate with each other.
-
+#
 # https://pypi.org/projects/requests/
 # 'requests' is a package that allows you to send HTTP requests in Python. 
 #     --> You can use it like a web browser to interact with web services and APIs.
-
+#
 # JSON (JavaScript Object Notation) is a data format that is easy to read and write for humans and machines.
 #     --> It is often used to exchange data between a server and a client in web applications. Or from different programming languages.
 
@@ -22,7 +25,7 @@ def v1():
         sys.exit(1)
 
     artist_name = sys.argv[1]
-    LIMIT = 2
+    LIMIT = 5
     url = f"https://itunes.apple.com/search?entity=song&limit={LIMIT}&term={artist_name}"
     
     response = requests.get(url)
@@ -36,8 +39,10 @@ def v1():
     o = response.json() # store the JSON response
     results = o.get('results', [])  # Use .get() to avoid KeyError if 'results' is not present
     
+    i = 1
     for result in results:
         trackName = result['trackName'] # return the value associated with the key 'trackName'
-        print(f"Track: {trackName}")
+        print(f"{i}. Track: {trackName}")
+        i += 1
 
 main()
